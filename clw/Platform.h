4a002c5cbd9e2b2593883ce9da6bc66d20c6a5a4
k0zmo <koozmoo@gmail.com>
2012-12-01 00:06:14 +0100

    added experimental method to check platform vendor

--- clw/Platform.h
+++ clw/Platform.h
@@ -11,6 +11,14 @@ namespace clw
 		Version_1_2 = 3
 	};
 
+	enum EPlatformVendor
+	{
+		Vendor_AMD,
+		Vendor_Intel,
+		Vendor_NVIDIA,
+		Vendor_Undefined
+	};
+
 	// Thin wrapper over OpenCL platform 
 	class Platform
 	{
@@ -27,6 +35,9 @@ namespace clw
 		string profile() const;
 		vector<string> extensions() const;
 
+		// Not fully tested - cl_khr_icd must be supported
+		inline EPlatformVendor vendorEnum() const;
+
 		cl_platform_id platformId() const { return id; }
 
 		// !TODO: unloadCompiler(); - only for 1.2
@@ -37,4 +48,17 @@ namespace clw
 
 	vector<Platform> availablePlatforms();
 	Platform defaultPlatform();
+
+	inline EPlatformVendor Platform::vendorEnum() const
+	{
+		string suffix = extensionSuffix();
+		if(suffix == "AMD")
+			return Vendor_AMD;
+		else if(suffix == "INTEL")
+			return Vendor_Intel;
+		else if(suffix == "NV")
+			return Vendor_NVIDIA;
+		else
+			return Vendor_Undefined;
+	}
 }
