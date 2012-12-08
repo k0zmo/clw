bedbbc17ccf07de640d04eded2fd7f64c9af9936
k0zmo <koozmoo@gmail.com>
2012-12-08 16:45:33 +0100

    updated premake file

--- premake4.lua
+++ premake4.lua
@@ -5,15 +5,15 @@ elseif os.get() == "linux"then
 end
 
 newoption {
-   trigger     = "clincdir",
-   value       = "PATH",
-   description = "Set path to a directory that contains CL/cl.h"
+	trigger     = "clincdir",
+	value       = "PATH",
+	description = "Set path to a directory that contains CL/cl.h"
 }
 
 newoption {
-   trigger     = "cllibdir",
-   value       = "PATH",
-   description = "Set path to a directory that contains OpenCL.lib or libOpenCL.so"
+	trigger     = "cllibdir",
+	value       = "PATH",
+	description = "Set path to a directory that contains OpenCL.lib or libOpenCL.so"
 }
 
 _OPTIONS["clincdir"] = _OPTIONS["clincdir"] or "$(AMDAPPSDKROOT)/include"
@@ -21,27 +21,37 @@ _OPTIONS["cllibdir"] = _OPTIONS["cllibdir"] or "$(AMDAPPSDKROOT)/lib/x86"
 
 solution "clw"
 	configurations { "Debug", "Release" }
-    project "clw"
-    	language "C++"
-	    location "proj"
-    	kind "StaticLib"
+	
+	--
+	-- Library itself
+	--
+	
+	project "clw"
+		language "C++"
+		location "proj"
+		kind "StaticLib"
 		targetdir "lib"
 		objdir "obj"
 		defines "CL_USE_DEPRECATED_OPENCL_1_1_APIS"
 		includedirs { _OPTIONS["clincdir"] }
-    	files { "clw/*.cpp", "clw/*.h" }
+		-- libdirs  { _OPTIONS["cllibdir"] }
+		files { "clw/*.cpp", "clw/*.h" }
 
 	configuration "Debug"
 		targetsuffix "_d"
 		defines { "DEBUG", "_DEBUG", }
-		flags "Symbols"
+		flags { "Symbols", "ExtraWarnings" }
 	
 	configuration "Release"
-		defines "NDEBUG"	
-		flags { "OptimizeSpeed", "NoEditAndContinue", "NoFramePointer" }
+		defines "NDEBUG"
+		flags { "OptimizeSpeed", "NoEditAndContinue", "NoFramePointer", "ExtraWarnings" }
 	
 	configuration { "linux", "gmake" }
-		buildoptions "-std=c++0x" 
+		buildoptions "-std=c++11" 
+		
+	--
+	-- Simple tool to query basic platform and its devices attributes
+	--
 		
 	project "clwinfo"
 		language "C++"
@@ -58,11 +68,11 @@ solution "clw"
 	configuration "Debug"
 		targetsuffix "_d"
 		defines { "DEBUG", "_DEBUG", }
-		flags "Symbols"
+		flags { "Symbols", "ExtraWarnings" }
 		
 	configuration "Release"
-		defines "NDEBUG"	
-		flags { "OptimizeSpeed", "NoEditAndContinue", "NoFramePointer" }
+		defines "NDEBUG"
+		flags { "OptimizeSpeed", "NoEditAndContinue", "NoFramePointer", "ExtraWarnings" }
 	
 	configuration { "linux", "gmake" }
-		buildoptions  "-std=c++0x"   
+		buildoptions  "-std=c++11"   
