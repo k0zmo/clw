126569813203906f16147d167b94d51adfa51c6a
k0zmo <koozmoo@gmail.com>
2013-04-29 23:33:04 +0200

    updated project file to build for x64 architecture

--- premake4.lua
+++ premake4.lua
@@ -17,9 +17,9 @@ newoption {
 }
 
 newoption {
-    trigger     = "static",
-    value       = "no",
-    description = "Build as static library?"
+    trigger     = "cllib64dir",
+    value       = "PATH",
+    description = "Set path to a directory that contains OpenCL.lib or libOpenCL.so for x86_64 architecture"
 }
 
 newoption {
@@ -28,41 +28,40 @@ newoption {
     description = "Enables OpenCL 1.2 features"
 }
 
-_OPTIONS["static"] = _OPTIONS["static"] or "no"
 _OPTIONS["opencl12"] = _OPTIONS["opencl12"] or "no"
 
 -- Default paths on windows for AMD APP SDK
 if os.get() == "windows" then 
     _OPTIONS["clincdir"] = _OPTIONS["clincdir"] or "$(AMDAPPSDKROOT)/include"
     _OPTIONS["cllibdir"] = _OPTIONS["cllibdir"] or "$(AMDAPPSDKROOT)/lib/x86"
+	_OPTIONS["cllib64dir"] = _OPTIONS["cllib64dir"] or "$(AMDAPPSDKROOT)/lib/x86_64"
 end
 
 -- Common settings per project
 local function configureProject()
-    configuration "Debug"
-        targetsuffix "_d"
-        defines { "DEBUG", "_DEBUG", }
-        flags { 
-            "Symbols", 
-            "ExtraWarnings"
-        }
-        
-    configuration "Release"
-        defines "NDEBUG"
-        flags { 
-            "OptimizeSpeed",
-            "NoEditAndContinue", 
-            "NoFramePointer", 
-            "ExtraWarnings"
-        }
-    
-    configuration { "linux", "gmake" }
-        buildoptions  "-std=c++11"   
-        links "stdc++"
+	configuration "Debug or DebugLib"
+		targetsuffix "_d"
+		defines { "DEBUG", "_DEBUG", }
+		flags { 
+			"Symbols", 
+			"ExtraWarnings"
+		}
+	configuration "Release or ReleaseLib"
+		defines "NDEBUG"
+		flags { 
+			"OptimizeSpeed",
+			"NoEditAndContinue", 
+			"NoFramePointer", 
+			"ExtraWarnings"
+		}
+	configuration { "linux", "gmake" }
+		buildoptions  "-std=c++11"   
+		links "stdc++"
 end
 
 solution "clw"
-    configurations { "Debug", "Release" }
+    configurations { "Debug", "Release", "DebugLib", "ReleaseLib" }
+	platforms { "x32", "x64" }
 
     -- Available in premake4.4
     if vpaths ~= nil then
@@ -79,16 +78,12 @@ solution "clw"
     project "clw"
         language "C++"
         location "proj"
-        if _OPTIONS["static"] == "yes" then
-            kind "StaticLib"
-            defines "CLW_STATIC_LIB"
-        else
-            kind "SharedLib"
-            defines "CLW_BUILD_SHARED"
-            links "OpenCL"
-        end
-        targetdir "bin"
         objdir "obj"
+		includedirs { _OPTIONS["clincdir"] }  
+        files { 
+            "clw/*.cpp",
+            "clw/*.h" 
+        }
         defines {
             -- needed in AMD's OpenCL headers to enable still used functions (such as clCreateImage2D)
             "CL_USE_DEPRECATED_OPENCL_1_1_APIS", 
@@ -97,14 +92,35 @@ solution "clw"
         if _OPTIONS["opencl12"] == "yes" then
             defines "HAVE_OPENCL_1_2"
         end
-        includedirs { _OPTIONS["clincdir"] }
-        libdirs { _OPTIONS["cllibdir"] }
-        files { 
-            "clw/*.cpp",
-            "clw/*.h" 
-        }
+		
+		-- linker settings for different architectures
+		configuration "x32"
+			libdirs { _OPTIONS["cllibdir"] }
+		configuration "x64"
+			libdirs { _OPTIONS["cllib64dir"] }		
+			
+		-- donfiguration for static builds
+		configuration "DebugLib or ReleaseLib"
+			kind "StaticLib"
+			defines "CLW_STATIC_LIB"
 
-        configureProject()
+		-- configuration for dll builds
+		configuration "Debug or Release"
+			kind "SharedLib"
+            defines "CLW_BUILD_SHARED"
+            links "OpenCL"
+			
+		-- output directory for different builds and architectures
+		configuration { "DebugLib or ReleaseLib", "x32" }
+			targetdir "lib"
+		configuration { "DebugLib or ReleaseLib", "x64" }
+			targetdir "lib64"
+		configuration { "Debug or Release", "x32" }
+			targetdir "bin"
+		configuration { "Debug or Release", "x64" }
+			targetdir "bin64"
+			
+		configureProject()
         
     --
     -- Simple tool to query basic platform and its devices attributes
@@ -114,23 +130,29 @@ solution "clw"
         language "C++"
         location "proj"
         kind "ConsoleApp"
-        targetdir "bin"
-        objdir "obj"
-        defines "HAVE_OPENCL_1_1"
-        if _OPTIONS["opencl12"] == "yes" then
-            defines "HAVE_OPENCL_1_2"
-        end
+		objdir "obj"
+        files "clwinfo/main.cpp"
         includedirs { 
             ".",
             _OPTIONS["clincdir"]
         }
-        files "clwinfo/main.cpp"
-        links "clw"
-        
-        if _OPTIONS["static"] == "yes" then
-            defines "CLW_STATIC_LIB"
-            links "OpenCL"
-            libdirs  { _OPTIONS["cllibdir"] }
+        defines "HAVE_OPENCL_1_1"
+        if _OPTIONS["opencl12"] == "yes" then
+            defines "HAVE_OPENCL_1_2"
         end
+		links "clw"
         
+		configuration "x32"
+			targetdir "bin"
+		configuration "x64"
+			targetdir "bin64"
+			
+		configuration "DebugLib or ReleaseLib"
+            defines "CLW_STATIC_LIB"
+			-- need to explicitly link OpenCL
+            links "OpenCL"
+			configuration "x32"
+				libdirs { _OPTIONS["cllibdir"] }
+			configuration "x64"
+				libdirs { _OPTIONS["cllib64dir"] }       
         configureProject()
